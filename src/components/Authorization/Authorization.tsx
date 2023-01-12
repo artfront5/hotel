216e10702392a59e30bd054f21577974e908e588
artfront7 <art.front@list.ru>
2023-01-13 00:02:32 +0300

    two pages

---
+++ src/components/Authorization/Authorization.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import css from './Authorization.module.scss';
+import './Authorization.css';
+
+const Authorization: React.FC = () => {
+  return <h1 className="color">Authorization</h1>;
+};
+export default Authorization;
