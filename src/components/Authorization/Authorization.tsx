5e0aba833f8e07242fe7b617a2854c9eebcfc8d2
artfront7 <art.front@list.ru>
2023-01-13 01:23:07 +0300

    fix scss

--- src/components/Authorization/Authorization.tsx
+++ src/components/Authorization/Authorization.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-// import css from './Authorization.module.scss';
-import './Authorization.css';
+import css from './Authorization.module.scss';
 
 const Authorization: React.FC = () => {
-  return <h1 className="color">Authorization</h1>;
+  return <div className={css.background}>login</div>;
 };
 export default Authorization;
