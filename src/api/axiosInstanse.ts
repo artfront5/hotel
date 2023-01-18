ae19dabec4dc566253625a080048c3cbf68b9b62
artfront7 <art.front@list.ru>
2023-01-18 10:00:15 +0300

    fix

--- src/api/axiosInstanse.ts
+++ src/api/axiosInstanse.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const instanceAxios = axios.create({
-  baseURL: 'http://engine.hotellook.com/api/v2/cache.json',
+  baseURL: 'https://engine.hotellook.com/api/v2/cache.json',
   timeout: 10000,
 });
